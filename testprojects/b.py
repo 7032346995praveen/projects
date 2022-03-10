4e960c840c94c205278bf2560519e0d6614ff9cc
praveen <68511537+7032346995praveen@users.noreply.github.com>
2022-03-10 21:20:45 +0530

    data types

---
+++ testprojects/b.py
@@ -0,0 +1,21 @@
+a="hello world !"
+print(a) #string
+print(type(a))
+b=20
+print(b) #int
+print(type(b))
+c=20.25
+print(c)#float
+print(type(c))
+d=1j
+print(d)# complex
+print(type(d))
+e=['apple','banana','cherry']
+print(e)#list
+print(type(e))
+f=("orange","mango","cherry")
+print(f)
+print(type(f)) #dictionary
+g={"name":"john","age":36}
+print(g)
+print(type(g))
